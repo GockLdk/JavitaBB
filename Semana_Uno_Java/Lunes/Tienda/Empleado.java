8c1a1da06b2cb9f03a4346567516184d18f3377a
Mario <marioalverto543@hotmail.com>
2018-09-17 22:56:20 -0500

    empleados

---
+++ Semana_Uno_Java/Lunes/Tienda/Empleado.java
@@ -0,0 +1,39 @@
+public class Empleado
+{
+	private int salario;
+	private String nom;
+	private String puesto;
+
+
+	public int getSalario()
+	{
+		return salario;
+	}
+
+	public void setSalario(int salario)
+	{
+		this.salario = salario;
+	}
+
+	public String getNom()
+	{
+		return nom;
+	}
+
+	public void setNom(String nom)
+	{
+		this.nom = nom;
+	}
+
+	public String getPuesto()
+	{
+		return puesto;
+	}
+
+	public void setPuesto(String puesto)
+	{
+		this.puesto = puesto;
+	}
+
+
+}
