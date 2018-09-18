336402745a91fe1673a777c0a8d7d01b6258411d
Mario <marioalverto543@hotmail.com>
2018-09-17 22:48:26 -0500

    inicio de tienda

---
+++ Semana_Uno_Java/Lunes/Tienda/Producto.java
@@ -0,0 +1,26 @@
+public class Producto
+{
+	private int precio;
+	private String nom;
+
+	public int getPrecio()
+	{
+		return precio;
+	}
+
+	public void setPrecio(int precio)
+	{
+		this.precio = precio;
+	}
+
+	public String getNom()
+	{
+		return nom;
+	}
+
+	public void setNom(String nom)
+	{
+		this.nom = nom;
+	}
+
+}
