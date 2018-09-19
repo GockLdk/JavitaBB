d3dd899659d302a3ad70378a6cbfa9d8f6e86633
Mario <marioalverto543@hotmail.com>
2018-09-19 01:18:25 -0500

    calcu casi finish

--- Semana_Uno_Java/Martes/NumeroComplejo.java
+++ Semana_Uno_Java/Martes/NumeroComplejo.java
@@ -27,13 +27,13 @@ public class NumeroComplejo
     resta.parte_imaginaria = a.parte_imaginaria - b.parte_imaginaria;
     return resta;
   }
-  public NumeroComplejo multiplicar(NumeroComplejo a, NumeroComplejo b){
+  public static NumeroComplejo multiplicar(NumeroComplejo a, NumeroComplejo b){
     NumeroComplejo multiplicacion = new NumeroComplejo();
     multiplicacion.parte_real =a.parte_real*b.parte_real-a.parte_imaginaria*b.parte_imaginaria;
     multiplicacion.parte_imaginaria = a.parte_real*b.parte_imaginaria+a.parte_imaginaria*b.parte_real;
     return multiplicacion;
   }
-  public NumeroComplejo dividir(NumeroComplejo a, NumeroComplejo b){
+  public static NumeroComplejo dividir(NumeroComplejo a, NumeroComplejo b){
     NumeroComplejo division = new NumeroComplejo();
     double aux;
      if(b.modulo()==0.0)
@@ -49,7 +49,7 @@ public class NumeroComplejo
     return division;
   }
  
-  public double ang(NumeroComplejo a)
+  public static double ang(NumeroComplejo a)
   {
     double angulo = Math.atan2(a.parte_imaginaria,a.parte_real);  
     if(angulo<0){   
@@ -60,7 +60,7 @@ public class NumeroComplejo
   return angulo;
   }
   
-  public void cambiarNumeros(NumeroComplejo a, NumeroComplejo b){
+  public static void cambiarNumeros(NumeroComplejo a, NumeroComplejo b){
     NumeroComplejo aux = new NumeroComplejo();
     aux.parte_real = a.parte_real;
     a.parte_real = b.parte_real;
@@ -77,10 +77,21 @@ public class NumeroComplejo
     NumeroComplejo uno = new NumeroComplejo(1,3);
     NumeroComplejo dos = new NumeroComplejo(5,8);
     NumeroComplejo ax = new NumeroComplejo();
-    //Calculadora prebeCalculadora = new Calculadora(uno,dos);
+    
+    System.out.println("Numero uno: 1+3i");
+    System.out.println("Numero dos: 5+8i");
     ax=NumeroComplejo.sumar(uno,dos);
-    System.out.println(ax.parte_real + " + " + ax.parte_imaginaria);
-    System.out.println(NumeroComplejo.restar(uno,dos));
-
+    System.out.println("La suma es " +ax.parte_real + " + " + ax.parte_imaginaria+"i");
+    ax = NumeroComplejo.restar(uno,dos);
+    System.out.println("La resta es " + ax.parte_real  + ax.parte_imaginaria+"i");
+    ax = NumeroComplejo.multiplicar(uno,dos);
+    System.out.println("La multiplicacion es " + ax.parte_real + " + " + ax.parte_imaginaria+"i");
+    ax = NumeroComplejo.dividir(uno,dos);
+    System.out.println("La division es " + ax.parte_real + " + " + ax.parte_imaginaria+"i");
+    System.out.println("El angulo de 1+3i es: "+NumeroComplejo.ang(uno)+"°");
+    System.out.println("El angulo de 5+8i es: "+NumeroComplejo.ang(dos)+"°");
+    NumeroComplejo.cambiarNumeros(uno,dos);
+    System.out.println("Ahora uno es: " +uno.parte_real + " + " + uno.parte_imaginaria+"i");
+    System.out.println("Ahora dos es: " +dos.parte_real + " + " + dos.parte_imaginaria+"i");
   }
 }
